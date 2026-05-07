0ccd45105875f43b681d7d730af99ddaa642e70b
RITHISH M <rithishmuthusamys@gmail.com>
2026-05-07 06:27:52 +0530

    Update AutofixCompilerApplicationTests.java

--- backend/autofix-compiler/src/test/java/com/rithish/autofix/AutofixCompilerApplicationTests.java
+++ backend/autofix-compiler/src/test/java/com/rithish/autofix/AutofixCompilerApplicationTests.java
@@ -1,13 +1,9 @@
 package com.rithish.autofix;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class AutofixCompilerApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
